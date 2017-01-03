a583860854b4651b23052bd52d043a5b13d5f1ed
Alexander Fedyashov <af@e42.guru>
2017-01-03 17:52:12 +0200

    fix(Assertions): update function assert

---
+++ test/fixtures/skiped-arrow/actual.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Example extends Component {
+  render() {
+    return null;
+  }
+
+  renderItems = () => {
+    return null;
+  }
+}
